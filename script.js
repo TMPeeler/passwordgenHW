8c4f82d8af6b3c76d1a8c058e0d1b1f1ce2a44f7
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-03-25 15:58:33 -0700

    add basic ideas

---
+++ script.js
@@ -0,0 +1,30 @@
+//I am prompted to create a new password
+//Then I am prompted for the length of the password which should be no less than 8 characters and no more than 128 characters
+//Then I am prompted for character types to be used in the password which includes lowercase, uppercase, numbers, and special characters
+//Then I put in my user input
+//Then my input is validated and at least one character type should be used in the password
+//Then the password is generated
+//Then the password is displayed either in an alert or written into the page
+
+//check for length of password
+//check for character types
+//the for loop runs as many times as we give it characters in the password
+
+for (i = 0 )
+
+var lowercase = "cdefghijklmnopqrstuvwxyz".split('');
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+var numbers = "123456789".split('');
+var specialCharacter = "!@#$%^&*".split('');
+
+var passwordGenerator = function() {
+
+    var userPassword = window.prompt("Would you like to make a new password?");
+
+    if (!userPassword) {
+            return;
+
+        } else if {
+
+        }
+}
