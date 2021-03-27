dcd9e1fea7243441b3d663b46ba5891e9be7e023
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-03-26 23:01:48 -0700

    attempt to fix password generator

--- script.js
+++ script.js
@@ -24,18 +24,24 @@
 //if the user has not selected at least one character type, cancel. But if the user has selected at least one character type, proceed.
 //based on the criteria for the password, run a for loop as many times as there are characters in the password (variable of password length) 
 
+var generatorButton = document.querySelector("#button")
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["1","2","3","4","5","6","7","8","9"];
+var specialCharacter = ["!","@","#","$","%","^","&","*","[","{",",","}","]","'",":",";"];
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz".split('');
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-var numbers = "123456789".split('');
-var specialCharacter = "!@#$%^&*[{}]':;".split('');
-var printedPass = [];
+// var lowerCase = "abcdefghijklmnopqrstuvwxyz".split('');
+// var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+// var numbers = "123456789".split('');
+// var specialCharacter = "!@#$%^&*[{}]':;".split('');
+
 var allArrays = [].concat(lowerCase, upperCase, numbers, specialCharacter)
 
 
 //method that concats arrays together
 
 var passwordGenerator = function() {
+    var printedPass;
 // this part prompts the user to create a new password
     var userChoice = window.confirm("Would you like to make a new password?");
 // if the user presses cancel then the process ceases here
@@ -46,34 +52,72 @@ var passwordGenerator = function() {
     var lowercaseChoice = window.confirm("Would you like to include lowercase characters in your password?");
     var upperCaseChoice = window.confirm("Would you like to include uppercase characters in your password?");
     var numbersChoice = window.confirm("Would you like to include numbers in your password?");
-    var specialCharacterChoice = window.confirm("Would you like to include special characters in your password");
-    if (lowercaseChoice || upperCaseChoice || numbersChoice || specialCharacterChoice) {
+    var specialChoice = window.confirm("Would you like to include special characters in your password");
+    if (lowercaseChoice || upperCaseChoice || numbersChoice || specialChoice) {
     } else {
         window.alert("Please include at least one character type");
         return;
     }
+
 // as long as the user picked one of the character types, the password generator will ask how many characters they want
-// the boolean values have been stored and now the user is prompted to declare how many characters their password will be
+// the boolean values have been stored and now the user is prompted to declare how many characters their password will be\
+// I rewrote this part so it did the opposite thing. Originally if the password was 8 or more and 128 or less it would do the functions otherwise it would show the alert
+//Now it's setup so that if the password is LESS than 8 or MORE than 128 it will show the alert and return, otherwise it runs the rest of the code
     var passwordLength = window.prompt("How many characters would you like your password to be? Between 8 and 128 characters");
-     if (passwordLength >= 8 && passwordLength <= 128) {
-        window.passwordValue = passwordLength;
-        generatePassword();
-     }else{
-         window.alert("Password must be between 8 and 128 characters");
+     if (passwordLength < 8 || passwordLength > 128) {
+        window.alert("Password must be between 8 and 128 characters");
          return;
+    }else{
+     //check to see which parameters were chosen by the user and concat an array specifically for each set of options chosen
+     //This part was the hardest for me to understand until it was brought to my attention by multiple classmates that I had nothing to check for specific options and
+     //then I was forced to rewrite a large portion of my code once I was helped to this understanding
+     
+    if (lowercaseChoice && upperCaseChoice && numbersChoice && specialChoice) {
+            characterChoice = lowercaseChoice.concat(upperCaseChoice,numbersChoice,specialChoice);
+     } else if (lowercaseChoice && upperCaseChoice && numbersChoice) {
+         characterChoice = lowercaseChoice.concat(upperCaseChoice,numbersChoice);
+     } else if (lowercaseChoice && upperCaseChoice && specialChoice) {
+        characterChoice = lowercaseChoice.concat(upperCaseChoice,specialChoice);
+     }  else if (lowercaseChoice && numbersChoice && specialChoice) {
+            characterChoice = lowercaseChoice.concat(upperCaseChoice,specialChoice);
+     } else if (lowercaseChoice && upperCaseChoice) {
+         characterChoice = lowercaseChoice.concat(upperCaseChoice);
+     } else if (lowercaseChoice && specialCharacter) {
+         characterChoice = lowercaseChoice.concat(specialChoice);
+     } else if (lowercaseChoice) {
+         characterChoice = lowercaseChoice;
+
+
+         //any combo that has lowercase in it will work
+     } else if (upperCaseChoice && numbersChoice && specialChoice) {
+         characterChoice = upperCaseChoice.concat(numbersChoice, specialChoice);
+     } else if (upperCaseChoice && numbersChoice) {
+        characterChoice = upperCaseChoice.concat(numbersChoice);
+     } else if (upperCaseChoice && specialChoice) {
+        characterChoice = upperCaseChoice.concat(specialChoice);
+        //any combo that has uppercase in it will work
+     } else if (numbersChoice && lowercaseChoice) {
+         characterChoice = numbersChoice.concat(lowercaseChoice);
+     } else if (numbersChoice && specialChoice) {
+         characterChoice = numbersChoice.concat(specialChoice);
+     } else if (numbersChoice) {
+         characterChoice = numbersChoice;
+         //any combo that has numbers in it will work
+     } else if (specialChoice) {
+         characterChoice = specialChoice;
+     } else {
+         generatePassword();
      }
+    }
+    for (i = 0; i < passwordLength; i++){
+     printedPass += characterChoice[Math.floor(Math.random()*characterChoice.length)];
+    }
+    return printedPass;
 }
 // this part generates the password by saying that we're going to start at 0 and randomly pick indexes from a concated array of all my other arrays 
 // and it's going to continue to pick values 
 
 
-function generatePassword(allArrays) {
-    for(i = 0; i < passwordLength; i++) {
-        var randomPass = Math.floor(Math.random*(passwordLength)) //random number picker 
-        randomPass = allArrays[randomPass] // this says that my new variable is the index number of my allArrays array
-        passwordText.append(randomPass); //this puts my array number 
-    }
-}
 // function generatePassword(allArrays) {
 //     for(i = 0; i < passwordLength; i++) {
 //         var randomPass = Math.floor(Math.random*(passwordLength)) //random number picker 
@@ -81,7 +125,11 @@ function generatePassword(allArrays) {
 //         passwordText.append(randomPass); //this puts my array number 
 //     }
 // }
-//
+
+
+
+
+
 var passwordPrint = function() {
     var password = passwordGenerator();
     var passwordText = document.getElementById("#actualpassword");
@@ -89,4 +137,9 @@ var passwordPrint = function() {
 }
 passwordGenerator();
 
- console.log(typeof passwordLength)
+
+generatorButton.addEventListener("click", passwordPrint);
+
+
+ //console.log(printedPass)
+
